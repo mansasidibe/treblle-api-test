82b4cdb98dbfc6fc61968ef0963990cd16d70e94
Arouna Sidibé <sidibearouna71@gmail.com>
2022-06-17 11:29:03 +0000

    controllers & model op

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    //
+    public function view()
+    {
+        # code...
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+    }
+}
