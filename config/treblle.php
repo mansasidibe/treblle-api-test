d78797972e67160d9365f95c701b5e609fb87b4f
Arouna Sidibé <sidibearouna71@gmail.com>
2022-06-17 11:38:53 +0000

    fin configuration de treblle

---
+++ config/treblle.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // ...
+
+    /*
+     * Define which fields should be masked before leaving the server
+     */
+    'masked_fields' => [
+        'password',
+        'pwd',
+        'secret',
+        'password_confirmation',
+        'cc',
+        'card_number',
+        'ccv',
+        'ssn',
+        'credit_score',
+        'api_key',
+    ],
+];
